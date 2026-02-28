79563289957d4ac16b87d8ed6c751d2a4a3fcf5e
qwertz-dev <palau.dandak@hotmail.com>
2026-02-28 15:31:12 +0100

    Update main.c

--- source/main.c
+++ source/main.c
@@ -1,15 +1,17 @@
 #include <wups.h>
-#include <vpad/input.h>
 
+/**
+ * Ohne diese Zeilen gibt es immer Error 1.
+ * Wir halten es extrem simpel für den ersten Erfolg.
+ */
 WUPS_PLUGIN_NAME("King-Gamepad-vWii");
-WUPS_PLUGIN_DESCRIPTION("vWii GamePad Mod");
+WUPS_PLUGIN_DESCRIPTION("vWii GamePad Test");
 WUPS_PLUGIN_VERSION("1.0");
 WUPS_PLUGIN_AUTHOR("King");
 WUPS_PLUGIN_LICENSE("GPL");
 
-// Diese Funktion ist der Kern - wir lassen sie erst mal leer,
-// um zu testen, ob der Compiler überhaupt durchläuft
 INITIALIZE_PLUGIN() {
+    // Erst mal leer lassen, um Error 1 zu besiegen!
 }
 
 DEINITIALIZE_PLUGIN() {
